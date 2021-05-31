e31a2f2b0ba3003f143db21c9f97c8632f2a3402
isaac-lee-zhuan-jian.isaac-@students.mq.edu.au <53724604+IsaacZJMQ@users.noreply.github.com>
2021-05-31 16:41:57 +1000

    Updating Client.java
    Added variables

--- Client.java
+++ Client.java
@@ -3,6 +3,36 @@ import java.net.*;
 
 public class Client {
 //Declaring variables
+//Assigning localhost
+public static String hostName = "localhost";
+
+//Assigning server port number
+public static int serverPort = 50000;
+
+//Assigning username
+public static String userName = "user.name";
+
+//Start initialisation with HELO
+public static String HELO = "HELO";
+
+//Authentication username on the system
+public static String AUTHusername = "AUTH" + " " + System.getProperty(userName);
+
+//The Client is ready to read
+public static String REDY = "REDY";
+
+//Job Submission information
+public static String JOBN = "JOBN";
+
+//Scheduling decision
+public static String SCHD = "SCHD";
+
+//Job completion
+public static String JCPL = "JCPL";
+
+public static String OK = "OK";
+public static String JOBP = "JOBP";
+public static String QUIT = "QUIT";
 
 }
 
