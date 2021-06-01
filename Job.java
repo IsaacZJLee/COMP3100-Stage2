d2dad2e9681c85c84af73a5a2797f60d1af2c3f8
isaac-lee-zhuan-jian.isaac-@students.mq.edu.au <53724604+IsaacZJMQ@users.noreply.github.com>
2021-06-01 12:06:30 +1000

    Added and updated Job and Server files

--- Job.java
+++ Job.java
@@ -1,3 +1,17 @@
 public class Job {
-    
+    int startTime;
+    int jobID;
+    int runTime;
+    int core;
+    int memory;
+    int disk;
+
+    public Job(int startTime, int jobID, int runTime, int core, int memory, int disk) {
+        this.startTime = startTime;
+        this.jobID = jobID;
+        this.runTime = runTime;
+        this.core = core;
+        this.memory = memory;
+        this.disk = disk;
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,19 @@
 public class Server {
-    
+    String type;
+    int serverID;
+    String state;
+    int startTime;
+    int coreCount;
+    int memory;
+    int disk;
+
+    public Server (String type, int serverID, String state, int startTime, int coreCount, int memory, int disk) {
+        this.type = type;
+        this.serverID = serverID;
+        this.state = state;
+        this.startTime = startTime;
+        this.coreCount = coreCount;
+        this.memory = memory;
+        this.disk = disk;
+    }
 }
